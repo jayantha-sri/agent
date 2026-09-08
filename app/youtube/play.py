f37013877576a0bc250db3e93b76cb02b87a40e0
jayantha-sri <jayanthasri11@gmail.com>
2026-09-08 14:31:44 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -48,6 +48,24 @@ for pattern in patterns:
       pattern,
       text
   )
+  if match:
+
+   query = match.group(1)
+   break
+
+query = query.strip()
+
+video_id = get_vid(query)
+
+if not video_id:
+ return None
+
+return(
+ "https://www.youtube.com/embed/"
+ + video_id
+ + "?autoplay=1&mute=0"
+)
+
 
 
 
