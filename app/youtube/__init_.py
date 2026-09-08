811f5e9173a0560bee3d54e1e9c973681c4f08e0
jayantha-sri <jayanthasri11@gmail.com>
2026-09-08 11:37:41 +0530

    Update __init_.py

--- app/youtube/__init_.py
+++ app/youtube/__init_.py
@@ -1,1 +1,32 @@
+from flask import Blueprint , request , jsonify
+
+youtube_bp = blueprint(
+   " youtube ",
+   __name__
+)
+
+@youtube_bp.route(
+ "/play ",
+  methods =["POST"]
+
+)
+def play ():
+
+ data = request.get_json(
+   silent = True
+   )or {}
+
+command = data.get(
+ "command",
+  ""
+).strip() 
+ 
+if not command :
+  return jsonify({
+    "success":"false",
+    "message":"there is no song name,mentioned"
+  })400
+  
+   
+
 
